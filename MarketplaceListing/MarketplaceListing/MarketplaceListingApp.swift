5e1994c1f182ab88c10869fe6ee7f20ce57753f4
Hamza <>
2024-12-17 01:21:55 +0100

    project init

---
+++ MarketplaceListing/MarketplaceListing/MarketplaceListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketplaceListingApp.swift
+//  MarketplaceListing
+//
+//  Created by Hamza on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarketplaceListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarketplaceListing/MarketplaceListingTests/MarketplaceListingTests.swift
@@ -0,0 +1,16 @@
+//
+//  MarketplaceListingTests.swift
+//  MarketplaceListingTests
+//
+//  Created by Hamza on 17/12/2024.
+//
+
+import Testing
+
+struct MarketplaceListingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
